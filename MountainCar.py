464616a701744f6b502ade4d1845c5073823e573
nateoosterink <nateoosterink@gmail.com>
2023-11-27 10:23:40 -0800

    First

--- MountainCar.py
+++ MountainCar.py
@@ -8,16 +8,18 @@
 
 import gym
 import numpy as np
+import matplotlib.pyplot as plt
 
 env = gym.make("MountainCar-v0")
 
-LEARNING_RATE = 0.1
+LEARNING_RATE = 0.2
 
 DISCOUNT = 0.95
 EPISODES = 25000
 SHOW_EVERY = 3000
+STATS_EVERY = 100
 
-DISCRETE_OS_SIZE = [20, 20]
+DISCRETE_OS_SIZE = [40] * len(env.observation_space.high)
 discrete_os_win_size = (env.observation_space.high - env.observation_space.low)/DISCRETE_OS_SIZE
 
 # Exploration settings
@@ -29,6 +31,8 @@ epsilon_decay_value = epsilon/(END_EPSILON_DECAYING - START_EPSILON_DECAYING)
 
 q_table = np.random.uniform(low=-2, high=0, size=(DISCRETE_OS_SIZE + [env.action_space.n]))
 
+ep_rewards = []
+aggr_ep_rewards = {'ep': [], 'avg': [], 'max': [], 'min': []}
 
 def get_discrete_state(state):
     discrete_state = (state - env.observation_space.low)/discrete_os_win_size
@@ -38,7 +42,7 @@ def get_discrete_state(state):
 for episode in range(EPISODES):
     discrete_state = get_discrete_state(env.reset())
     done = False
-
+    episode_reward = 0
     if episode % SHOW_EVERY == 0:
         render = True
         print(episode)
@@ -56,7 +60,7 @@ for episode in range(EPISODES):
 
 
         new_state, reward, done, _ = env.step(action)
-
+        episode_reward += reward
         new_discrete_state = get_discrete_state(new_state)
 
         if episode % SHOW_EVERY == 0:
@@ -90,5 +94,22 @@ for episode in range(EPISODES):
     if END_EPSILON_DECAYING >= episode >= START_EPSILON_DECAYING:
         epsilon -= epsilon_decay_value
 
+    ep_rewards.append(episode_reward)
+    if not episode % STATS_EVERY:
+        average_reward = sum(ep_rewards[-STATS_EVERY:])/STATS_EVERY
+        aggr_ep_rewards['ep'].append(episode)
+        aggr_ep_rewards['avg'].append(average_reward)
+        aggr_ep_rewards['max'].append(max(ep_rewards[-STATS_EVERY:]))
+        aggr_ep_rewards['min'].append(min(ep_rewards[-STATS_EVERY:]))
+        print(f'Episode: {episode:>5d}, average reward: {average_reward:>4.1f}, current epsilon: {epsilon:>1.2f}')
+
+    np.save(f"qtables/{episode}-qtable.npy", q_table)
 
 env.close()
+
+plt.plot(aggr_ep_rewards['ep'], aggr_ep_rewards['avg'], label="average rewards")
+plt.plot(aggr_ep_rewards['ep'], aggr_ep_rewards['max'], label="max rewards")
+plt.plot(aggr_ep_rewards['ep'], aggr_ep_rewards['min'], label="min rewards")
+plt.legend(loc=4)
+plt.grid(True)
+plt.show()
